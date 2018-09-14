2db85b2a974b9bb8bf5169d9289880113364288e
Fynn <cx495086@outlook.com>
2018-09-14 21:48:08 +0800

    init success

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import {createStackNavigator } from 'react-navigation';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
+    
     'Shake or press menu button for dev menu',
 });
 
